852ca8c4ba247bcaf6db5ab07a8af200103430a7
Hyan18 <Hyan18@github.com>
2019-11-15 11:46:04 +0000

    test get_entires to locked diary

--- lib/secret_diary.rb
+++ lib/secret_diary.rb
@@ -2,4 +2,8 @@ class SecretDiary
   def add_entry
     return "Diary is locked"
   end
+
+  def get_entries
+    return "Diary is locked"
+  end
 end
--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -7,4 +7,11 @@ describe SecretDiary do
       expect(secret_diary.add_entry).to eq("Diary is locked")
     end
   end
+
+  describe '#get_entries' do
+    it "should throw an error when the diary is locked" do
+      secret_diary = SecretDiary.new
+      expect(secret_diary.get_entries).to eq("Diary is locked")
+    end
+  end
 end
